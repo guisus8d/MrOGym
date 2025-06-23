cc822a0854ff33e3c5859bc813c4af349e00feef
jesus jimenez martinez <jimenezmartinezjesus76@gmail.com>
2025-06-23 13:59:03 -0500

    agregar

--- .netlify/v1/functions/ssr/.netlify/build/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_BQEl44n7.mjs';
-export { renderers } from '../renderers.mjs';
--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import netlify from '@astrojs/netlify/functions'; // Cambiar a Netlify adapter
 
 export default defineConfig({
   output: 'server',
-  adapter: netlify(), // Usar el adaptador de Netlify
+  adapter: netlify(),
+  site: 'https://mrogym.netlify.app/', // AÑADE ESTO
+  base: '/',                          // AÑADE ESTO
   
   vite: {
     define: {
       'process.env.RESEND_API_KEY': JSON.stringify(process.env.RESEND_API_KEY),
-      'process.env.OTRA_VARIABLE': JSON.stringify(process.env.OTRA_VARIABLE),
     },
     build: {
       target: 'esnext',
